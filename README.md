8d09ae010eb0eef9e9e9be33d47531ffae4f65bd
Dejan Velimirović <dvsoftware@gmail.com>
2017-06-22 11:28:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# locker
+# Locker
 Simple Redis locking that doesn't use polling
+
+## How it works
+This library implements "reverse" locking, so instead of setting the lock key, the lock is acquired immediately upon calling the lock function by using [BLPOP](https://redis.io/commands/blpop). The critical process is started by pushing an item to the list defined as a lock key and the lock is released when the critical process promise is resolved. The promise is then resolved, and the next locked process continues execution.
+
+## Installation
+
+```bash
+npm install node-locker
+```
+
+## Usage
+
+```javasript
+const Locker = require('node-locker');
+const locker = new Locker({ /* redis config */ });
+
+locker.lock('myLockKey', () => {
+	// Some critical code here
+	// E.g. financial transactions
+	return Promise.resolve( /* result */ ); // Resolve the promise to release the lock
+}).then((result) => {
+	// Lock is released here
+});
+```
+
+## License
+```
+MIT License
+
+Copyright (c) 2017 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
---
+++ test/locker.test.js
@@ -0,0 +1,171 @@
+const Locker = require('../');
+const Redis = require('ioredis');
+
+const redis = new Redis();
+
+beforeEach(() => redis.del('locker:lock1', 'locker:lock2', 'locker:lock1:counter', 'locker:lock2:counter'));
+
+describe('Locker tests', () => {
+	it('should be a constructor', () => {
+		expect(typeof Locker).toBe('function');
+	});
+
+	it('should have the lock function', () => {
+		const locker = new Locker();
+		expect(typeof locker.lock).toBe('function');
+	});
+
+	it('should have the unlock function', () => {
+		const locker = new Locker();
+		expect(typeof locker.unlock).toBe('function');
+	});
+
+	it('should call unlock after processing', () => {
+		const locker = new Locker();
+		locker.unlock = jest.fn(locker.unlock);
+		return locker.lock('locker:lock1', () => {
+			return Promise.resolve({});
+		}).then(() => {
+			expect(locker.unlock).toBeCalled();
+		});
+		
+	});
+
+	it('should not unlock second lock before first lock', () => {
+		const locker = new Locker();
+		const resolve = jest.fn(() => Promise.resolve());
+		const unlock = jest.fn(() => Promise.resolve());
+		return Promise.all([
+			locker.lock('locker:lock1', () => {
+				return new Promise(resolve => {
+					setTimeout(() => {
+						expect(unlock).not.toBeCalled();
+					}, 500);
+					setTimeout(() => {
+						resolve();
+					}, 1000);
+				})
+			}),
+			locker.lock('locker:lock1', resolve).then(unlock)
+		]);
+	});
+
+	it('should not process second call until the first is unlocked', () => {
+		const locker = new Locker();
+		const resolve = jest.fn(() => Promise.resolve());
+		return Promise.all([
+			locker.lock('locker:lock1', () => {
+				return new Promise(res => {
+					setTimeout(() => {
+						expect(resolve).not.toBeCalled();
+					}, 500);
+					setTimeout(() => {
+						res();
+					}, 1000);
+				}).then(resolve);
+			}),
+			locker.lock('locker:lock1', resolve).then(() => {
+				expect(resolve.mock.calls.length).toBe(2);
+			})
+		]);
+	});
+
+	it('should unlock both locks', () => {
+		const locker = new Locker();
+		const resolve = jest.fn(() => Promise.resolve());
+		const unlock = jest.fn(() => Promise.resolve());
+
+		return Promise.all([
+			locker.lock('locker:lock1', resolve).then(unlock),
+			locker.lock('locker:lock1', resolve).then(unlock),
+		])
+			.then(() => {
+				expect(unlock.mock.calls.length).toBe(2);
+			})
+		
+	});
+
+	it('should not block another key', () => {
+		const locker = new Locker();
+		const resolve = jest.fn(() => Promise.resolve());
+		const unlock = jest.fn(() => Promise.resolve());
+
+		return Promise.all([
+			locker.lock('locker:lock1', () => {
+				return new Promise(res => {
+					setTimeout(() => {
+						expect(unlock).toBeCalled();
+					}, 500);
+					setTimeout(() => {
+						res();
+					}, 1000);
+				})
+			}),
+			locker.lock('locker:lock2', resolve).then(unlock)
+		]);
+	});
+
+	it('should unlock in sequence', () => {
+		const locker = new Locker();
+		const unlock = jest.fn(() => Promise.resolve());
+		const unlock1 = jest.fn(() => Promise.resolve());
+		const unlock2 = jest.fn(() => Promise.resolve());
+
+		const process = jest.fn((timeout) => () => new Promise(resolve => {
+			setTimeout(resolve, timeout);
+		}));
+		
+		setTimeout(() => { 
+			expect(unlock).toBeCalled()
+			expect(unlock1).not.toBeCalled()
+			expect(unlock2).not.toBeCalled()
+		}, 1100);
+		setTimeout(() => { 
+			expect(unlock).toBeCalled()
+			expect(unlock1).toBeCalled()
+			expect(unlock2).not.toBeCalled()
+		}, 2100);
+		setTimeout(() => { 
+			expect(unlock).toBeCalled()
+			expect(unlock1).toBeCalled()
+			expect(unlock2).toBeCalled()
+		}, 3100);
+
+		locker.lock('locker:lock1', process(1000)).then(unlock);
+		locker.lock('locker:lock1', process(1001)).then(unlock1);
+		locker.lock('locker:lock1', process(1002)).then(unlock2);
+
+		return new Promise((resolve) => {
+			setTimeout(resolve, 3200);
+		})
+	
+	});
+
+	it('should unlock in lock order', () => {
+		const locker = new Locker();
+		const resolve = jest.fn((id) => {
+			console.log(id);	
+			return Promise.resolve(id)
+		});
+		const unlock = jest.fn(() => Promise.resolve());
+		return Promise.all([
+			locker.lock('locker:lock1', resolve.bind(null, 1)).then(unlock),
+			locker.lock('locker:lock1', resolve.bind(null, 2)).then(unlock),
+			locker.lock('locker:lock1', resolve.bind(null, 3)).then(unlock),
+		]).then(() => {
+			expect(unlock.mock.calls[0][0]).toBe(1);
+			expect(unlock.mock.calls[1][0]).toBe(2);
+			expect(unlock.mock.calls[2][0]).toBe(3);
+		});
+	});
+
+	it('should pass the result to the promise', () => {
+		const locker = new Locker();
+		return locker.lock('locker:lock1', () => {
+			return Promise.resolve('test');
+		}).then((result) => {
+			expect(result).toBe('test');
+		});
+	});
+	
+});
